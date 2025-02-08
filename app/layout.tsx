80101f76680ab54514e01cc69904cce50dd700f1
Anish V Mathew <anishvmathew02@gmail.com>
2025-02-08 11:16:00 +0530

    added prloader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,23 @@
+"use client";
+import { useState } from "react";
 import AuthProvider from "@/components/AuthProvider";
 import "./globals.css";
+import DearCareLogo from "@/components/logo";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(true);
+
   return (
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" href="/DearCare.png" sizes="32x32" />
       </head>
       <body>
-        <AuthProvider>{children}</AuthProvider>
+        {loading ? (
+          <DearCareLogo onComplete={() => setLoading(false)} />
+        ) : (
+          <AuthProvider>{children}</AuthProvider>
+        )}
       </body>
     </html>
   );
